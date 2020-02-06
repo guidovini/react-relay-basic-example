998983e98612f59f85ca0fd81082bdc9947adacb
Guido Santillan <guidosantillan01@gmail.com>
2020-02-06 16:22:40 +0100

    Render AppQuery

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import graphql from 'babel-plugin-relay/macro';
+import { QueryRenderer } from 'react-relay';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import environment from './environment';
+
+const query = graphql`
+  query AppQuery {
+    jobs {
+      id
+      title
+      cities(where: { name: "Berlin" }) {
+        name
+        country {
+          name
+          isoCode
+        }
+      }
+      tags {
+        name
+      }
+      applyUrl
+      company {
+        name
+        websiteUrl
+        logoUrl
+      }
+      updatedAt
+      postedAt
+    }
+  }
+`;
+
+const renderQuery = ({ error, props }) => {
+  if (error) {
+    return <div>Error!</div>;
+  }
+  if (!props) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(props);
+
+  return <div>Query fetched</div>;
+};
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={query}
+        variables={{}}
+        render={renderQuery}
+      />
+    );
+  }
 }
-
-export default App;
