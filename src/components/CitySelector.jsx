c103bfc51264c0e5daf72821859b9101b0513c45
Guido Santillan <guidosantillan01@gmail.com>
2020-02-07 19:22:50 +0100

    Add state and handlers to CitySelector

--- src/components/CitySelector.jsx
+++ src/components/CitySelector.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const CitySelector = ({ cities }) => (
-  <select>
-    {cities.map((city) => (
-      <option value={city.slug} key={city.slug}>
-        {city.name}
-      </option>
-    ))}
-  </select>
-);
+const CitySelector = ({ cities }) => {
+  const [selectedCity, setSelectedCity] = useState('berlin');
+
+  useEffect(() => {
+    console.log('You changed the city: ', selectedCity);
+  }, [selectedCity]);
+
+  return (
+    <select
+      value={selectedCity}
+      onChange={(e) => setSelectedCity(e.target.value)}
+    >
+      {cities.map((city) => (
+        <option value={city.slug} key={city.slug}>
+          {city.name}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 export default CitySelector;
