26839c35dcd342c3c3ab12348cfe701db61e778d
Guido Santillan <guidosantillan01@gmail.com>
2020-02-07 18:41:58 +0100

    Add file supported file extensions to relay.config.js

--- relay.config.js
+++ relay.config.js
@@ -3,6 +3,6 @@ module.exports = {
   // and `babel-plugin-relay`.
   src: './src/components',
   schema: './data/schema.graphql',
-  extensions: 'js jsx',
+  extensions: ['js', 'jsx'],
   exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
 };
